ea8f21ea6e8bc88a219e3158878d0a361d7a86de
xiaochangfeng <xcfouo@163.com>
2026-08-20 22:19:41 +0800

    test: make concurrency regression deterministic

--- internal/store/postgres/file_store_bug_test.go
+++ internal/store/postgres/file_store_bug_test.go
@@ -1,18 +1,33 @@
 package postgres
 
 import (
+	"chunked-upload-service/internal/domain"
 	"context"
 	"sync"
 	"testing"
 	"time"
-	"chunked-upload-service/internal/domain"
 )
 
 func TestSaveFileConcurrentUpdates(t *testing.T) {
 	r := NewMemoryRepo()
-	f := &domain.FileRecord{ID:"f",SessionID:"s",Name:"x",UploadedAt:time.Now()}
-	if err:=r.CreateFile(context.Background(),f);err!=nil{t.Fatal(err)}
+	f := &domain.FileRecord{ID: "f", SessionID: "s", Name: "x", UploadedAt: time.Now()}
+	if err := r.CreateFile(context.Background(), f); err != nil {
+		t.Fatal(err)
+	}
 	var wg sync.WaitGroup
-	for i:=0;i<16;i++{wg.Add(1);go func(){defer wg.Done();if err:=r.SaveFile(context.Background(),f);err!=nil{t.Error(err)}}()}
+	start := make(chan struct{})
+	for i := 0; i < 256; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			<-start
+			for j := 0; j < 100; j++ {
+				if err := r.SaveFile(context.Background(), f); err != nil {
+					t.Error(err)
+				}
+			}
+		}()
+	}
+	close(start)
 	wg.Wait()
 }
